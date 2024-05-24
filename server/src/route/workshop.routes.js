4f5535ff2dae44fc0459e3f36abe32d03cf1b233
Mathijs04 <m.vanengelen4@student.avans.nl>
2024-05-24 12:48:38 +0200

    Added packages. Commented out lines in workshop routes and services so app won't crash

--- server/src/route/workshop.routes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const logger = require("../util/logger");
-
-router.post("/api/workshop", workshopController.createWorkshop);
-
-module.exports = router;
--- server/src/service/workshop.service.js
+++ server/src/service/workshop.service.js
@@ -2,18 +2,18 @@ const database = require('../database/database.connection');
 const logger = require('../util/logger');
 
 const workshopService = {
-  create: (workshop, callback) => {
-    logger.info('creating workshop', workshop);
+//   create: (workshop, callback) => {
+//     logger.info('creating workshop', workshop);
 
-    database.getConnection(function (err, connection) {
-      if (err) {
-        logger.error('Error creating workshop', err);
-        callback(err, null);
-        return;
-      }
+//     database.getConnection(function (err, connection) {
+//       if (err) {
+//         logger.error('Error creating workshop', err);
+//         callback(err, null);
+//         return;
+//       }
 
-    });
-  },
+//     });
+//   },
 
   
 };
