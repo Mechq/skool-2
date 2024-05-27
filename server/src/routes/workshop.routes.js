b84d04bde07b04791aa46aea2c00ced0d2af8cbb
Mathijs04 <m.vanengelen4@student.avans.nl>
2024-05-27 10:07:56 +0200

    Almost finished api for adding workshop. Only need to finish query

--- server/src/routes/workshop.routes.js
+++ server/src/routes/workshop.routes.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const logger = require("../util/logger");
 const workshopController = require("../controller/workshop.controller");
 
-// router.post("/api/workshop", workshopController.createWorkshop);
+router.post("/api/workshop", workshopController.createWorkshop);
 
 module.exports = router;
