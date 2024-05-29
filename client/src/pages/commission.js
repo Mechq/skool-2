695a04deb4be2c205cca77f432c2b068b7a3caba
Mech <stefrensma@gmail.com>
2024-05-29 10:47:55 +0200

    Create button with sidepanel

--- client/src/pages/commission.js
+++ client/src/pages/commission.js
@@ -1,13 +1,27 @@
 import React, {useEffect, useState} from "react";
 import CommissionList from "../components/lists/CommissionList";
+import CreateWorkshopButton from "../components/CreateWorkshopButton";
+import MailTemplateList from "../components/lists/MailTemplateList";
+import SidePanel from "../components/SidePanel";
+import MailTemplateContent from "../components/panel-contents/MailTemplateContent";
 
 
 export default function Commission() {
-
+    const [showSidePanel, setShowSidePanel] = useState(false);
+    const [sidePanelContent, setSidePanelContent] = useState("");
     return (
-        <div>
+        <>
             <h1>Opdracht</h1>
             <CommissionList />
-        </div>
+
+            <CreateWorkshopButton
+                setShowSidePanel={setShowSidePanel}
+                showSidePanel={showSidePanel}
+                setSidePanelContent={setSidePanelContent}
+                />
+            <SidePanel showSidePanel={showSidePanel}>
+                {sidePanelContent === "create" && <MailTemplateContent/>}
+            </SidePanel>
+        </>
     );
 }
