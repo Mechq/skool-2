8ca85705b2925f2ee2738fb83710f0c72c904db5
Mech <stefrensma@gmail.com>
2024-05-29 12:43:08 +0200

    refactoring

--- server/src/routes/category.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const logger = require("../util/logger");
-const categoryController = require("../controller/category.controller");
-
-router.get("/api/category", categoryController.getAllCategories);
-
-module.exports = router;
--- server/src/routes/commission.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const logger = require("../util/logger");
-const commissionController = require("../controller/commission.controller");
-
-router.get("/api/commission",commissionController.getAllCommissions);
-router.post("/api/commission", commissionController.createCommission);
-
-module.exports = router;
