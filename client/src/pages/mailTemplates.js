7ddff7fd740015346725b98b784927accbecfd94
Mech <stefrensma@gmail.com>
2024-05-29 13:45:52 +0200

    merge EditMailTemplateContent.jsx

--- client/src/pages/mailTemplates.js
+++ client/src/pages/mailTemplates.js
@@ -3,6 +3,7 @@ import MailTemplateList from "../components//lists/MailTemplateList";
 import SidePanel from "../components/SidePanel";
 import MailTemplateContent from "../components/panel-contents/MailTemplateContent";
 import CreateButton from "../components/CreateButton";
+import EditMailTemplateContent from "../components/panel-contents/EditMailTemplateContent";
 
 export default function MailTemplates() {
     const [showSidePanel, setShowSidePanel] = useState(false);
@@ -23,7 +24,8 @@ export default function MailTemplates() {
 
             <SidePanel showSidePanel={showSidePanel}>
                 {sidePanelContent === "create" && <MailTemplateContent/>}
-                {sidePanelContent === "edit" && <MailTemplateContent/>}
+                {sidePanelContent === "edit" &&
+                    <EditMailTemplateContent mailTemplateId={mailTemplateId} setShowSidePanel={setShowSidePanel}/>}
             </SidePanel>
             <MailTemplateList
                 setShowSidePanel={setShowSidePanel}
