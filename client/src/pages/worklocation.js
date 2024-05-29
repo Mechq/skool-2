ed4ba1e2bad70af2cf4f30b6be15ee9480138d1c
dymok <dymokilan@gmail.com>
2024-05-29 13:46:41 +0200

    minor edits

--- client/src/pages/worklocation.js
+++ client/src/pages/worklocation.js
@@ -1,29 +1,27 @@
 import React, { useEffect, useState } from "react";
 import SidePanel from "../components/SidePanel";
-// import LocationList from "../components/LocationList"; // Assuming you have a LocationList component
-import '../styles/werklocatie.css';
+import CreateWorkLocationContent from "../components/panel-contents/CreateWorkLocationContent";
 
-function Werklocatie() {
+function Worklocation() {
     const [showSidePanel, setShowSidePanel] = useState(false);
     const [sidePanelContent, setSidePanelContent] = useState("");
-    const [workshopId, setWorkshopId] = useState(null);
 
     useEffect(() => {
         console.log("Side panel state: ", showSidePanel);
     }, [showSidePanel]);
 
     return (
-        <div className='werklocatieContent'>
+        <div>
             <button className="fab fab-common" onClick={() => setShowSidePanel(!showSidePanel)}>
                 <span className={showSidePanel ? "rotate" : ""}>{'+'}</span>
             </button>
             {/* <LocationList /> Assuming you have a LocationList component */}
             <h1 className="title">Werklocatie Aanmaken</h1>
-            <SidePanel showSidePanel={showSidePanel} setShowSidePanel={setShowSidePanel}>
-
+            <SidePanel showSidePanel={showSidePanel}>
+                {sidePanelContent === "create" && <CreateWorkLocationContent/>}
             </SidePanel>
         </div>
     );
 }
 
-export default Werklocatie;
+export default Worklocation;
