d9f7914342fef4108ec3ff7e516e1c757b95eaee
dymok <dymokilan@gmail.com>
2024-05-30 14:49:22 +0200

    addede host refresh fix

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,10 +1,6 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/", (req, res, next) => {
-    res.render('index.html')
-});
-
 router.get("/workshops", (req, res, next) => {
     res.render('index.html')
 });
@@ -12,5 +8,23 @@ router.get("/workshops", (req, res, next) => {
 router.get("/", (req, res, next) => {
     res.render('index.html')
 });
+
+router.get('/mailTemplates', (req, res, next) => {
+    res.render('index.html')
+});
+
+router.get('/opdracht', (req, res, next) => {
+    res.render('index.html')
+});
+
+router.get('/werklocatie', (req, res, next) => {
+    res.render('index.html')
+});
+
+router.get('/customers', (req, res, next) => {
+    res.render('index.html')
+});
+
+
 
 module.exports = router;
