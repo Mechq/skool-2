bb969225661eb6df48375ae2fc4d479e7df4b775
dymok <dymokilan@gmail.com>
2024-05-30 21:53:58 +0200

    converted SidePanel.jsx

--- client/src/components/SidePanel.jsx
+++ client/src/components/SidePanel.jsx
@@ -1,5 +1,4 @@
 import React, {useEffect, useRef} from "react";
-import "../styles/components/SidePanel.css";
 
 function SidePanel({showSidePanel, children}) {
     const sidePanelRef = useRef(null);
@@ -12,7 +11,7 @@ function SidePanel({showSidePanel, children}) {
 
     return (
         <div id='side-panel-root'>
-            <div ref={sidePanelRef} className="side-panel">
+            <div ref={sidePanelRef} className="h-[calc(100vh-90px)] w-[30vw] fixed z-10 bottom-0 right-0 bg-gray-200 overflow-x-hidden pt-5 transition-all">
                 {children}
             </div>
         </div>
