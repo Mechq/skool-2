b2fdab9d01fdae77c3f87bb06f109f1e026907a6
dymok <dymokilan@gmail.com>
2024-05-31 11:19:56 +0200

    added animation to side panel

--- client/src/components/SidePanel.jsx
+++ client/src/components/SidePanel.jsx
@@ -1,42 +1,30 @@
-import React, {useRef} from "react";
+import React from "react";
 
-function SidePanel({isOpen, setIsOpen,  rotateSpan, setRotateSpan, children}) {
-    const sidePanelRef = useRef(null);
+function SidePanel({isOpen, setIsOpen, rotateSpan, setRotateSpan, children}) {
 
     return (
         <>
-            {isOpen && (
-                <div
+            <div
+                className={
+                    "fixed overflow-hidden z-10 bg-gray-900 bg-opacity-25 inset-0 transform ease-in-out " +
+                    (isOpen
+                        ? "transition-opacity opacity-100 duration-500 "
+                        : "transition-opacity duration-500 opacity-0 pointer-events-none")
+                }
+            >
+                <section
                     className={
-                        " fixed overflow-hidden z-10 bg-gray-900 bg-opacity-25 inset-0 transform ease-in-out " +
-                        (isOpen
-                            ? " transition-opacity opacity-100 duration-500 "
-                            : " transition-opacity duration-500 opacity-0 ")
+                        "w-screen max-w-lg right-0 absolute bg-white h-full shadow-xl delay-400 duration-500 ease-in-out transform " +
+                        (isOpen ? "translate-x-0 " : "translate-x-full")
                     }
                 >
-                    <section
-                        className={
-                            " w-screen max-w-lg right-0 absolute bg-white h-full shadow-xl delay-400 duration-500 ease-in-out transition-all transform  " +
-                            (isOpen ? " translate-x-0 " : " translate-x-full ")
-                        }
-                    >
-                        <article className="relative w-screen max-w-lg pb-10 flex flex-col space-y-6 overflow-y-scroll h-full">
-                            <header className="p-4 font-bold text-lg">Header</header>
-                            {children}
-                        </article>
-                    </section>
-                </div>
-            )}
+                    <article className="relative w-screen max-w-lg pb-10 flex flex-col space-y-6 overflow-y-scroll h-full">
+                        <header className="p-4 font-bold text-lg">Header</header>
+                        {children}
+                    </article>
+                </section>
+            </div>
         </>
-
-
-
-        //
-        // <div id='side-panel-root'>
-        //     <div ref={sidePanelRef} className="h-[calc(100vh-90px)] w-[30vw] fixed z-10 bottom-0 right-0 bg-gray-200 overflow-x-hidden pt-5 transition-all">
-        //         {children}
-        //     </div>
-        // </div>
     );
 }
 
