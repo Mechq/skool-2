195a1ebec5a851c8fa21c20569efa1a88f3d87f6
Mech <stefrensma@gmail.com>
2024-05-31 10:33:30 +0200

    Update commission

--- src/controller/commission.controller.js
+++ src/controller/commission.controller.js
@@ -73,6 +73,29 @@ let commissionController = {
                 });
             }
         });
+    },
+    update: (req, res, next) => {
+        const commission = req.body;
+        const id = req.params.id;
+        logger.info('updating commission', commission);
+
+        commissionService.update(commission, id, (error, success) => {
+            if (error) {
+                return next({
+                    status: error.status,
+                    message: error.message,
+                    data: {},
+                });
+            }
+
+            if (success) {
+                res.status(200).json({
+                    status: success.status,
+                    message: success.message,
+                    data: success.data,
+                });
+            }
+        });
     }
 };
 
--- src/routes/commission.routes.js
+++ src/routes/commission.routes.js
@@ -5,5 +5,5 @@ const commissionController = require("../controller/commission.controller");
 router.get("/api/commission",commissionController.getAllCommissions);
 router.post("/api/commission", commissionController.createCommission);
 router.get("/api/commission/:id", commissionController.getCommissionById);
-
+router.put("/api/commission/:id", commissionController.update);
 module.exports = router;
