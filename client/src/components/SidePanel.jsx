b9dab2403ca16b9c76ad62af30b0186f758cc856
dymok <dymokilan@gmail.com>
2024-05-31 10:50:33 +0200

    working tailwindcss sidepanel, no animations yet

--- client/src/components/SidePanel.jsx
+++ client/src/components/SidePanel.jsx
@@ -1,35 +1,36 @@
-import React, {useEffect, useRef} from "react";
+import React, {useRef} from "react";
 
 function SidePanel({isOpen, setIsOpen, children}) {
     const sidePanelRef = useRef(null);
 
     return (
-        <div
-    className={
-        " fixed overflow-hidden z-10 bg-gray-900 bg-opacity-25 inset-0 transform ease-in-out " +
-        (isOpen
-            ? " transition-opacity opacity-100 duration-500 "
-            : " transition-opacity duration-500 opacity-0 ")
-    }
->
-    <section
-        className={
-            " w-screen max-w-lg right-0 absolute bg-white h-full shadow-xl delay-400 duration-500 ease-in-out transition-all transform  " +
-            (isOpen ? " translate-x-0 " : " translate-x-full ")
-        }
-    >
-        <article className="relative w-screen max-w-lg pb-10 flex flex-col space-y-6 overflow-y-scroll h-full">
-            <header className="p-4 font-bold text-lg">Header</header>
-            {children}
-        </article>
-    </section>
-    <section
-        className=" w-screen h-full cursor-pointer "
-        onClick={() => {
-            setIsOpen(false);
-        }}
-    ></section>
-</div>
+        <>
+            {isOpen && (
+                <div
+                    className={
+                        " fixed overflow-hidden z-10 bg-gray-900 bg-opacity-25 inset-0 transform ease-in-out " +
+                        (isOpen
+                            ? " transition-opacity opacity-100 duration-500 "
+                            : " transition-opacity duration-500 opacity-0 ")
+                    }
+                    onClick={() => {
+                        setIsOpen(false);
+                    }}
+                >
+                    <section
+                        className={
+                            " w-screen max-w-lg right-0 absolute bg-white h-full shadow-xl delay-400 duration-500 ease-in-out transition-all transform  " +
+                            (isOpen ? " translate-x-0 " : " translate-x-full ")
+                        }
+                    >
+                        <article className="relative w-screen max-w-lg pb-10 flex flex-col space-y-6 overflow-y-scroll h-full">
+                            <header className="p-4 font-bold text-lg">Header</header>
+                            {children}
+                        </article>
+                    </section>
+                </div>
+            )}
+        </>
 
 
 
