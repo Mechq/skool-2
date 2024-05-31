70aadf83fe83d222308c49f7bb0ff35d3406b945
Mathijs04 <m.vanengelen4@student.avans.nl>
2024-05-31 15:49:50 +0200

    Created function to get all workshops from a round

--- src/routes/workshopRound.routes.js
+++ src/routes/workshopRound.routes.js
@@ -4,4 +4,6 @@ const workshopRoundController = require("../controller/workshopRound.controller"
 
 
 router.post("/api/workshopRound/:workshopId/:roundId", workshopRoundController.create);
+router.get("/api/workshopRound/workshop/:roundId", workshopRoundController.getWorkshopsRoundById);
+
 module.exports = router;
--- src/service/workshopRound.service.js
+++ src/service/workshopRound.service.js
@@ -53,139 +53,34 @@ const workshopRound = {
         });
     },
 
+    getWorkshopsRoundById: (roundId, callback) => {
+        logger.info('Get all workshops from round', roundId);
 
-    // getAll: (callback) => {
-    //     logger.info('get all commissions');
-    //
-    //     database.getConnection(function (err, connection) {
-    //         if (err) {
-    //             logger.error('Error getting commission', err);
-    //             callback(err, null);
-    //             return;
-    //         }
-    //
-    //
-    //         connection.query(
-    //             'SELECT * FROM commission',
-    //             function (error, results, fields) {
-    //                 connection.release();
-    //
-    //                 if (error) {
-    //
-    //                     // TODO: Implement correct logging for possible error cases
-    //                     logger.error('Error getting commission', error);
-    //                     callback(error, null);
-    //
-    //                 } else {
-    //                     callback(null, {
-    //                         status: 200,
-    //                         message: `${results.length} commission retrieved`,
-    //                         data: results,
-    //                     });
-    //                 }
-    //             }
-    //         )
-    //     });
-    // },
-    //
-    // getCommissionById: (id, callback) => {
-    //     logger.info('getting commission by id', id);
-    //
-    //     let sql = 'SELECT * FROM commission WHERE id = ?';
-    //
-    //     database.query(sql, [id], (error, results, fields) => {
-    //         if (error) {
-    //             logger.error('Error getting commission', error);
-    //             callback(error, null);
-    //
-    //         } else {
-    //             if (results.length > 0) {
-    //                 logger.info('Commission fetched successfully', results[0]);
-    //                 callback(null, {
-    //                     status: 200,
-    //                     message: 'Commission fetched successfully',
-    //                     data: results[0],
-    //                 });
-    //             } else {
-    //                 logger.warn('No commission found with id', id);
-    //                 callback({
-    //                     status: 404,
-    //                     message: 'Commission not found',
-    //                 }, null);
-    //             }
-    //         }
-    //     });
-    // },
-    // update:
-    //     (commission, commissionId, callback) => {
-    //         logger.info('updating commission', commission);
-    //
-    //         let sql = 'UPDATE commission SET ';
-    //         const values = [];
-    //
-    //         if (commission.customerId) {
-    //             sql += 'customerId = ?, ';
-    //             values.push(commission.customerId);
-    //         }
-    //         if (commission.details) {
-    //             sql += 'details = ?, ';
-    //             values.push(commission.details);
-    //         }
-    //         if (commission.targetAudience) {
-    //             sql += 'targetAudience = ?, ';
-    //             values.push(commission.targetAudience);
-    //         }
-    //         // Remove the trailing comma and space
-    //         sql = sql.slice(0, -2);
-    //
-    //         sql += ' WHERE id = ?';
-    //         values.push(commissionId);
-    //
-    //         database.query(sql, values, (error, results, fields) => {
-    //             if (error) {
-    //                 logger.error('Error updating commission', error);
-    //                 callback(error, null);
-    //
-    //             } else {
-    //                 if (results.affectedRows > 0) {
-    //                     logger.info('commission updated successfully');
-    //                     callback(null, 'commission updated successfully');
-    //                 } else {
-    //                     logger.info('No commission found with the provided ID');
-    //                     callback(null, 'No commission found with the provided ID');
-    //                 }
-    //             }
-    //         });
-    //     },
-    //
-    // getCustomer: (customerId, callback) => {
-    //     logger.info('getting customer by id', customerId);
-    //
-    //     let sql = 'SELECT name FROM customer WHERE id = (SELECT customerId FROM commission WHERE id = ?)';
-    //
-    //     database.query(sql, [customerId], (error, results, fields) => {
-    //         if (error) {
-    //             logger.error('Error getting customer', error);
-    //             callback(error, null);
-    //
-    //         } else {
-    //             if (results.length > 0) {
-    //                 logger.info('Customer fetched successfully', results[0]);
-    //                 callback(null, {
-    //                     status: 200,
-    //                     message: 'Customer fetched successfully',
-    //                     data: results[0],
-    //                 });
-    //             } else {
-    //                 logger.warn('No customer found with id', customerId);
-    //                 callback({
-    //                     status: 404,
-    //                     message: 'Customer not found',
-    //                 }, null);
-    //             }
-    //         }
-    //     });
-    // }
+        let sql = 'SELECT workshop.* FROM workshop JOIN workshopRound ON workshop.id = workshopRound.workshopId WHERE workshopRound.roundId = ?';
+
+        database.query(sql, [roundId], (error, results, fields) => {
+            if (error) {
+                logger.error('Error getting workshops', error);
+                callback(error, null);
+
+            } else {
+                if (results.length > 0) {
+                    logger.info('Workshops fetched successfully', results[0]);
+                    callback(null, {
+                        status: 200,
+                        message: 'Workshops fetched successfully',
+                        data: results,
+                    });
+                } else {
+                    logger.warn('No workshops found with round id', roundId);
+                    callback({
+                        status: 404,
+                        message: 'workshops not found',
+                    }, null);
+                }
+            }
+        });
+    }
 
 };
 
