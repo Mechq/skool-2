142274b179d8d6ad495efd59ae4120cd5074103b
dymok <dymokilan@gmail.com>
2024-05-31 09:40:23 +0200

    navbar styling fixed

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -9,10 +9,11 @@ function NavBar() {
     };
 
     return (
-        <nav className="bg-white border-b border-gray-200 dark:border-gray-700 px-2 sm:px-4 py-2.5 dark:bg-gray-800 shadow">
+        <nav className="bg-white text-black border-b border-gray-200 px-2 sm:px-4 py-2.5 shadow">
             <div className="container mx-auto flex flex-wrap justify-between items-center">
                 <Link to="/" className="flex items-center">
-                    <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Website</span>
+                    <img src="https://skoolworkshop.nl/wp-content/uploads/2020/06/Skool-Workshop_Logo-200x65.png"
+                         alt="Logo" className="self-center h-10"/>
                 </Link>
                 <div className="flex items-center md:hidden">
                     <button
@@ -23,7 +24,8 @@ function NavBar() {
                     >
                         <span className="sr-only">Open main menu</span>
                         <svg className="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16m-7 6h7" />
+                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
+                                  d="M4 6h16M4 12h16m-7 6h7"/>
                         </svg>
                     </button>
                 </div>
@@ -40,16 +42,16 @@ function NavBar() {
     );
 }
 
-function CustomLink({ to, children, ...props }) {
+function CustomLink({to, children, ...props}) {
     const resolvedPath = useResolvedPath(to);
     const isActive = useMatch({ path: resolvedPath.pathname, end: true });
 
     return (
-        <li className={isActive ? 'text-blue-700' : 'text-gray-700'}>
+        <li className={isActive ? 'text-[#F49700]' : 'text-gray-700'}>
             <Link
                 to={to}
                 {...props}
-                className="block py-2 pr-4 pl-3 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                className="block py-2 pr-4 pl-3 hover:text-[#F49700] hover:underline md:hover:bg-transparent md:border-0 md:p-0"
             >
                 {children}
             </Link>
