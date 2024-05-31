416d72b8c89779b2970f83893df3cbe9548568b5
MikailSari1 <m.sari1@student.avans.nl>
2024-05-31 14:59:33 +0200

    Update mailTemplates.js

--- client/src/pages/mailTemplates.js
+++ client/src/pages/mailTemplates.js
@@ -1,14 +1,70 @@
-import React, {useEffect, useState} from "react";
-import MailTemplateList from "../components//lists/MailTemplateList";
+// import React, {useEffect, useState} from "react";
+// import MailTemplateList from "../components//lists/MailTemplateList";
+// import SidePanel from "../components/SidePanel";
+// import MailTemplateContent from "../components/panel-contents/MailTemplateContent";
+// import CreateButton from "../components/CreateButton";
+// import EditMailTemplateContent from "../components/panel-contents/EditMailTemplateContent";
+
+
+
+// export default function MailTemplates() {
+//     const [isOpen, setIsOpen] = React.useState(false);
+//     const [sidePanelContent, setSidePanelContent] = useState("");
+//     const [mailTemplateId, setMailTemplateId] = useState(null);
+//     const [mailTemplates, setMailTemplates] = useState([]);
+//     const [rotateSpan, setRotateSpan] = useState(false);
+
+//     useEffect(() => {
+//         fetch('/api/mailTemplate/')
+//             .then(res => res.json())
+//             .then(data => {
+//                 setMailTemplates(data.data);
+//                 console.log("Fetched mailTemplates: ", data.data);
+//             })
+//             .catch(error => console.error('Error fetching data:', error));
+//     }, [isOpen]);
+
+//     return (
+//         <div>
+//             <h1 id={"header"}>Mail Templates</h1>
+//             <CreateButton
+//                 setShowSidePanel={setIsOpen}
+//                 showSidePanel={isOpen}
+//                 setSidePanelContent={setSidePanelContent}
+//                 rotateSpan={rotateSpan}
+//                 setRotateSpan={setRotateSpan}
+//             />
+
+//             <SidePanel isOpen={isOpen}
+//                        setIsOpen={setIsOpen}
+//                        rotateSpan={rotateSpan}
+//                        setRotateSpan={setRotateSpan}>
+//                 {sidePanelContent === "create" &&
+//                     <MailTemplateContent  setShowSidePanel={setIsOpen}/>}
+//                 {sidePanelContent === "edit" &&
+//                     <EditMailTemplateContent mailTemplateId={mailTemplateId} setShowSidePanel={setIsOpen}/>}
+//             </SidePanel>
+//             <MailTemplateList
+//                 setShowSidePanel={setIsOpen}
+//                 showSidePanel={isOpen}
+//                 setSidePanelContent={setSidePanelContent}
+//                 setMailTemplateId={setMailTemplateId}
+//                 MailTemplates={mailTemplates}
+//                 setRotateSpan={setRotateSpan}
+//             />
+//         </div>
+//     );
+// }
+
+import React, { useEffect, useState } from "react";
+import MailTemplateList from "../components/lists/MailTemplateList";
 import SidePanel from "../components/SidePanel";
 import MailTemplateContent from "../components/panel-contents/MailTemplateContent";
 import CreateButton from "../components/CreateButton";
 import EditMailTemplateContent from "../components/panel-contents/EditMailTemplateContent";
-import EditPanelWorkshopContent from "../components/panel-contents/EditPanelWorkshopContent";
-import WorkshopList from "../components/lists/WorkshopList";
 
 export default function MailTemplates() {
-    const [isOpen, setIsOpen] = React.useState(false);
+    const [isOpen, setIsOpen] = useState(false);
     const [sidePanelContent, setSidePanelContent] = useState("");
     const [mailTemplateId, setMailTemplateId] = useState(null);
     const [mailTemplates, setMailTemplates] = useState([]);
@@ -26,7 +82,7 @@ export default function MailTemplates() {
 
     return (
         <div>
-            <h1 id={"header"}>Workshops</h1>
+            <h1 id="header">Mail Templates</h1>
             <CreateButton
                 setShowSidePanel={setIsOpen}
                 showSidePanel={isOpen}
@@ -40,16 +96,16 @@ export default function MailTemplates() {
                        rotateSpan={rotateSpan}
                        setRotateSpan={setRotateSpan}>
                 {sidePanelContent === "create" &&
-                    <MailTemplateContent  setShowSidePanel={setIsOpen}/>}
+                    <MailTemplateContent setShowSidePanel={setIsOpen} />}
                 {sidePanelContent === "edit" &&
-                    <EditMailTemplateContent mailTemplateId={mailTemplateId} setShowSidePanel={setIsOpen}/>}
+                    <EditMailTemplateContent mailTemplateId={mailTemplateId} setShowSidePanel={setIsOpen} />}
             </SidePanel>
-            <WorkshopList
+            <MailTemplateList
                 setShowSidePanel={setIsOpen}
                 showSidePanel={isOpen}
                 setSidePanelContent={setSidePanelContent}
-                setWorkshopId={setMailTemplateId}
-                workshops={mailTemplates}
+                setMailTemplateId={setMailTemplateId}
+                mailTemplates={mailTemplates} // Fix: ensure prop name is correct
                 setRotateSpan={setRotateSpan}
             />
         </div>
