f4527930b9d3974ae6da1e5aa8a62e91588b43ec
Mathijs04 <m.vanengelen4@student.avans.nl>
2024-06-03 12:15:08 +0200

    changed route for getting all rounds from commission

--- src/routes/round.routes.js
+++ src/routes/round.routes.js
@@ -2,9 +2,10 @@ const express = require("express");
 const router = express.Router();
 const roundController = require("../controller/round.controller");
 
-router.get("/api/round/:id", roundController.getAllRoundsFromCommission);
+router.get("/api/round/commission/:id", roundController.getAllRoundsFromCommission);
 router.post("/api/round/:id", roundController.createRound);
 router.delete("/api/round/:id", roundController.deleteRound);
 router.put("/api/round/:id", roundController.editRound);
+router.get("/api/round/:id", roundController.getRoundById);
 
 module.exports = router;
