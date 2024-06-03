6e10d016cdd72f601994e474ed25c5f41434c452
dymok <dymokilan@gmail.com>
2024-06-03 11:45:00 +0200

    login page done

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, useLocation } from 'react-router-dom';
 import NavBar from './components/NavBar';
-import Home from './pages/home';
 import Customers from './pages/customers';
 import Workshop from './pages/workshop';
 import MailTemplates from './pages/mailTemplates';
@@ -10,19 +9,29 @@ import Worklocation from './pages/worklocation';
 import Login from './components/LoginPage';
 
 function App() {
+    const location = useLocation();
+    const isLoginPage = location.pathname === "/";
+
+    React.useEffect(() => {
+        if (isLoginPage) {
+            document.body.style.overflow = 'hidden';
+        } else {
+            document.body.style.overflow = 'auto';
+        }
+    }, [isLoginPage]);
+
     return (
         <div className="flex flex-col min-h-screen">
-            <NavBar />
+            {!isLoginPage && <NavBar />}
             <div className="container mx-auto flex-grow py-4">
                 <Routes>
                     <Route path="*" element={<h1>Not Found</h1>} />
-                    <Route path="/" element={<Home />} />
+                    <Route path="/" element={<Login />} />
                     <Route path="/workshops" element={<Workshop />} />
                     <Route path="/mailTemplates" element={<MailTemplates />} />
                     <Route path="/opdracht" element={<Commission />} />
                     <Route path="/werklocatie" element={<Worklocation />} />
                     <Route path="/customers" element={<Customers />} />
-                    <Route path="/login" element={<Login />} />
                 </Routes>
             </div>
         </div>
