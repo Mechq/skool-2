54042dece7205dc4a8c050abccc8b70b95ed62c7
Mathijs04 <m.vanengelen4@student.avans.nl>
2024-06-04 11:33:22 +0200

    added function to change user data

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -22,6 +22,31 @@ let userController = {
                 });
             }
         });
+    },
+
+    updateUser: (req, res, next) => {
+        const id = req.params.id;
+        const user = req.body;
+
+        logger.info('updating user', id);
+
+        userService.update(id, user, (error, success) => {
+            if (error) {
+                return next({
+                    status: error.status,
+                    message: error.message,
+                    data: {},
+                });
+            }
+
+            if (success) {
+                res.status(200).json({
+                    status: success.status,
+                    message: success.message,
+                    data: success.data,
+                });
+            }
+        });
     }
 };
 
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const userController = require("../controller/user.controller");
 
 router.get("/api/user", userController.getAllUsers);
+router.put("/api/user/:id", userController.updateUser);
 
 module.exports = router;
