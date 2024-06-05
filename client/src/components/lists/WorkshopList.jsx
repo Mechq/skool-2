f599a3b0bbc4034eb765c8fe9e70a0a64c7f3d89
MikailSari1 <m.sari1@student.avans.nl>
2024-06-05 16:25:48 +0200

    SeeWorkshopDetails completed

--- client/src/components/lists/WorkshopList.jsx
+++ client/src/components/lists/WorkshopList.jsx
@@ -1,20 +1,24 @@
 import React, { useState, useEffect } from "react";
 import DeleteConfirmationModalScreen from "../modal-screens/DeleteConfirmationModalScreen";
+import WorkshopDetailsModalScreen from "../modal-screens/WorkshopDetailsModalScreen";
 
 export default function WorkshopList({
-                                         isOpen,
-                                         setIsOpen,
-                                         setSidePanelContent,
-                                         setWorkshopId,
-                                         workshops,
-                                         setRotateSpan,
-                                         setWorkshops
-                                     }) {
+    isOpen,
+    setIsOpen,
+    setSidePanelContent,
+    setWorkshopId,
+    workshops,
+    setRotateSpan,
+    setWorkshops
+}) {
     const [showModal, setShowModal] = useState(false);
+    const [showDetailsModal, setShowDetailsModal] = useState(false);
     const [workshopToDeleteId, setWorkshopToDeleteId] = useState(null);
     const [workshopToDeleteName, setWorkshopToDeleteName] = useState(null);
+    const [selectedWorkshopId, setSelectedWorkshopId] = useState(null); // Add state for selected workshop ID
+    const [selectedWorkshopName, setSelectedWorkshopName] = useState(null); // Add state for selected workshop name
 
-        useEffect(() => {
+    useEffect(() => {
         fetch('/api/workshop')
             .then(res => res.json())
             .then(data => {
@@ -24,22 +28,33 @@ export default function WorkshopList({
             .catch(error => console.error('Error fetching data:', error));
     }, [isOpen]); // Add isOpen as a dependency
 
-    const editWorkshop = (id) => {
+    const editWorkshop = (id, e) => {
+        e.preventDefault();
+        e.stopPropagation(); // Prevent row click event
         setWorkshopId(id);
         setSidePanelContent("edit");
         setIsOpen(true);
         setRotateSpan(true);
     };
 
+    const handleDetailsClick = (id, name, e) => {
+        e.preventDefault();
+        setSelectedWorkshopId(id); // Set the selected workshop ID
+        setSelectedWorkshopName(name); // Set the selected workshop name
+        setShowDetailsModal(true);
+    };
+
     const handleDeleteClick = (id, name, e) => {
         e.preventDefault();
+        e.stopPropagation(); // Prevent row click event
         setWorkshopToDeleteId(id);
-        setWorkshopToDeleteName(name)
+        setWorkshopToDeleteName(name);
         setShowModal(true);
     };
 
     const handleModalClose = () => {
         setShowModal(false);
+        setShowDetailsModal(false);
     };
 
     const handleModalSave = (editedType) => {
@@ -50,45 +65,45 @@ export default function WorkshopList({
         <div className="relative overflow-x-auto shadow-md sm:rounded-lg mr-6 ml-6">
             <table className="w-full text-sm text-left rtl:text-right text-gray-500 light:text-gray-400">
                 <thead className="text-xs text-gray-700 uppercase bg-gray-50 light:bg-gray-700 light:text-gray-400">
-                <tr>
-                    <th className="px-6 py-3">Workshop Naam</th>
-                    <th className=""></th>
-                    <th className="px-6 py-3">Categorie</th>
-                    <th className="px-6 py-3">Datum Aangemaakt</th>
-                    <th className="px-6 py-3">Bewerken</th>
-                    <th></th>
-                </tr>
+                    <tr>
+                        <th className="px-6 py-3">Workshop Naam</th>
+                        <th className=""></th>
+                        <th className="px-6 py-3">Categorie</th>
+                        <th className="px-6 py-3">Datum Aangemaakt</th>
+                        <th className="px-6 py-3">Bewerken</th>
+                        <th></th>
+                    </tr>
                 </thead>
                 <tbody>
-                {workshops.map(workshop => (
-                    <tr key={workshop.id}
-                        className="odd:bg-white odd:light:bg-gray-900 even:bg-gray-50 even:light:bg-gray-800 border-b light:border-gray-700">
-                        <td className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap light:text-white">
-                            {workshop.name}
-                        </td>
-                        <td className="px-6 py-4">{workshop.subject}</td>
-                        <td className="px-6 py-4">{workshop.category}</td>
-                        {/*<td className="px-6 py-4">{workshop.createdAt}</td>*/}
-                        <td className="px-6 py-4">-</td>
-                        <td className="px-6 py-4">
-                            <a href="#" onClick={(e) => {
-                                e.preventDefault();
-                                editWorkshop(workshop.id);
-                            }} className="font-medium text-[#f49700] light:text-[#f49700] hover:underline">Bewerken</a>
-                        </td>
-                        <td className="px-">
-                            <a href="#" onClick={(e) => handleDeleteClick(workshop.id, workshop.name, e)}>
-                                <svg className="w-5 h-5 mx-2 text-danger hover:text-red-600" aria-hidden="true"
-                                     xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none"
-                                     viewBox="0 0 24 24">
-                                    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round"
-                                          strokeWidth="2"
-                                          d="M5 7h14m-9 3v8m4-8v8M10 3h4a1 1 0 0 1 1 1v3H9V4a1 1 0 0 1 1-1ZM6 7h12v13a1 1 0 0 1-1 1H7a1 1 0 0 1-1-1V7Z"/>
-                                </svg>
-                            </a>
-                        </td>
-                    </tr>
-                ))}
+                    {workshops.map(workshop => (
+                        <tr key={workshop.id}
+                            className="odd:bg-white odd:light:bg-gray-900 even:bg-gray-50 even:light:bg-gray-800 border-b light:border-gray-700 hover:cursor-pointer"
+                            onClick={(e) => handleDetailsClick(workshop.id, workshop.name, e)}>
+                            <td className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap light:text-white">
+                                {workshop.name}
+                            </td>
+                            <td className="px-6 py-4">{workshop.subject}</td>
+                            <td className="px-6 py-4">{workshop.category}</td>
+                            {/* <td className="px-6 py-4">{workshop.createdAt}</td> */}
+                            <td className="px-6 py-4">-</td>
+                            <td className="px-6 py-4">
+                                <a href="#" onClick={(e) => {
+                                    editWorkshop(workshop.id, e);
+                                }} className="font-medium text-[#f49700] light:text-[#f49700] hover:underline">Bewerken</a>
+                            </td>
+                            <td className="px-">
+                                <a href="#" onClick={(e) => handleDeleteClick(workshop.id, workshop.name, e)}>
+                                    <svg className="w-5 h-5 mx-2 text-danger hover:text-red-600" aria-hidden="true"
+                                        xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none"
+                                        viewBox="0 0 24 24">
+                                        <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round"
+                                            strokeWidth="2"
+                                            d="M5 7h14m-9 3v8m4-8v8M10 3h4a1 1 0 0 1 1 1v3H9V4a1 1 0 0 1 1-1ZM6 7h12v13a1 1 0 0 1-1 1H7a1 1 0 0 1-1-1V7Z"/>
+                                    </svg>
+                                </a>
+                            </td>
+                        </tr>
+                    ))}
                 </tbody>
             </table>
 
@@ -102,6 +117,17 @@ export default function WorkshopList({
                     />
                 </div>
             )}
+
+            {showDetailsModal && (
+                <div>
+                    <WorkshopDetailsModalScreen
+                        onClose={handleModalClose}
+                        onSave={handleModalSave}
+                        workshopId={selectedWorkshopId}
+                    />
+                </div>
+            )}
+
         </div>
     );
 }
---
+++ client/src/components/modal-screens/WorkshopDetailsModalScreen.jsx
@@ -0,0 +1,41 @@
+import React, { useEffect, useState } from "react";
+
+export default function WorkshopDetailsModalScreen({ onClose, onSave, workshopId, }) {
+    const [workshop, setWorkshop] = useState({});
+        
+    
+    console.log("workshopId", workshopId);
+
+    useEffect(() => {
+        fetch(`/api/workshop/${workshopId}`)
+            .then(res => res.json())
+            .then(data => {
+                setWorkshop(data.data);
+                console.log("Fetched workshop: ",workshop);
+            })
+            .catch(error => console.error('Error fetching data:', error));
+    }, [workshopId]);
+
+
+    return (
+        <>
+            <div className="fixed inset-0 z-10 bg-gray-900 bg-opacity-15" onClick={onClose} />
+            <section className="w-full h-full flex items-center justify-center fixed top-0 left-0 z-20">
+                <div className="flex flex-col items-center justify-center w-3/4 h-3/4 max-w-4xl">
+                    <div className="w-full bg-white rounded-lg shadow light:border light:bg-gray-800 light:border-gray-700">
+                        <div className="p-6 space-y-4 sm:space-y-6">
+                            <span className="close text-xl cursor-pointer" onClick={onClose}>&times;</span>
+                            <h1 className="text-xl font-medium m-0 p-0" style={{ marginTop: 0 }}><strong>{workshop.name}</strong></h1>
+                            <h2 className=""><strong>Naam:</strong> <br />
+                                {workshop.name}</h2>
+                            <h2 className=""><strong>Details:</strong> <br />
+                                {workshop.description}</h2>
+                                <h2 className=""><strong>Materialen:</strong> <br />
+                                {workshop.materials}</h2>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </>
+    );
+}
