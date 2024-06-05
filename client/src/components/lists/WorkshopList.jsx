820937e9402e9d9ed1f092401d84962bb4ef33a6
dymok <dymokilan@gmail.com>
2024-06-05 12:03:21 +0200

    minor changes

--- client/src/components/lists/WorkshopList.jsx
+++ client/src/components/lists/WorkshopList.jsx
@@ -77,7 +77,7 @@ export default function WorkshopList({
                         </td>
                         <td className="px-">
                             <a href="#" onClick={(e) => handleDeleteClick(workshop.id, workshop.name, e)}>
-                                <svg className="w-5 h-5 mx-1 text-danger hover:text-red-600" aria-hidden="true"
+                                <svg className="w-5 h-5 mx-2 text-danger hover:text-red-600" aria-hidden="true"
                                      xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none"
                                      viewBox="0 0 24 24">
                                     <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round"
