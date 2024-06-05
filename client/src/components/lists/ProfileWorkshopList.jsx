d423a7253b7b439f9e5bedc1a0103239d3841de8
dymok <dymokilan@gmail.com>
2024-06-05 16:49:27 +0200

    wrong working profileworkshopList

---
+++ client/src/components/lists/ProfileWorkshopList.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+
+export default function ProfileWorkshopList({user, editUser, workshops}) {
+    return (
+        <>
+            <div className="justify-center">
+                <div className="bg-white max-w-2xl shadow overflow-hidden sm:rounded-lg">
+                    <div className="px-4 py-5 sm:px-6">
+                        <h3 className="text-lg leading-6 font-medium text-gray-900">
+                            Workshops
+                        </h3>
+                        <p className="mt-1 max-w-2xl text-sm text-gray-500">
+                            Welke workshops kan ik aanbieden?
+                        </p>
+                    </div>
+                    <div className="border-t border-gray-200">
+                        <table className="min-w-full divide-y divide-gray-200">
+                            <thead className="bg-gray-50">
+                            <tr>
+                                <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                    Name
+                                </th>
+                                <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                    Category
+                                </th>
+                                <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                </th>
+                            </tr>
+                            </thead>
+                            <tbody className="bg-white divide-y divide-gray-200">
+                            {workshops.map(workshop => (
+                                <tr key={workshop.id}
+                                    className="wfiodd:bg-white odd:light:bg-gray-900 even:bg-gray-50 even:light:bg-gray-800 border-b light:border-gray-700">
+                                    <td className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap light:text-white">
+                                        {workshop.name}
+                                    </td>
+                                    <td className="px-6 py-4">{workshop.category}</td>
+                                    <td className="px-6 py-4 text-center">
+                                        <input id={`checkbox-${workshop.id}`} type="checkbox" value=""
+                                               className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 light:focus:ring-blue-600 light:ring-offset-gray-800 focus:ring-2 light:bg-gray-700 light:border-gray-600"/>
+                                    </td>
+                                </tr>
+                            ))}
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+
+            {/* Bewerk button */}
+            <button
+                // onClick={editUser}
+                className="bg-brand-orange hover:bg-brand-orange-hover focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-white mt-4"
+            >
+                Update
+            </button>
+        </>
+    )
+}
--- client/src/pages/user.js
+++ client/src/pages/user.js
@@ -2,12 +2,18 @@ import React, {useEffect, useState} from "react";
 import SidePanel from "../components/SidePanel";
 import EditPanelWorkshopContent from "../components/panel-contents/EditPanelWorkshopContent";
 import PageSecurity from "../PageSecurity";
+import UserProfile from "../components/lists/UserProfile";
+import ProfileWorkshopList from "../components/lists/ProfileWorkshopList";
 
 function User() {
     const [isOpen, setIsOpen] = React.useState(false);
     const [sidePanelContent, setSidePanelContent] = useState("");
     const [user, setUser] = useState({});
     const [rotateSpan, setRotateSpan] = useState(false);
+    const [workshopId, setWorkshopId] = useState(null);
+    const [workshops, setWorkshops] = useState([]);
+    const [loading, setLoading] = useState(true);
+
     const userEmail = PageSecurity();
 
     useEffect(() => {
@@ -17,16 +23,32 @@ function User() {
                 .then(data => {
                     setUser(data.data);
                     console.log("Fetched profile: ", data.data);
+                    // Set loading to false once data is fetched
+                    setLoading(false);
+                })
+                .catch(error => {
+                    console.error('Error fetching data:', error);
+                    // Also set loading to false in case of error
+                    setLoading(false);
+                });
+
+            fetch(`/api/teacherWorkshopQualification/${user.id}`)
+                .then(res => res.json())
+                .then(data => {
+                    setWorkshops(data.data);
+                    console.log("Fetched workshops: ", data.data);
                 })
                 .catch(error => console.error('Error fetching data:', error));
         }
-    }, [userEmail]);
-
+    }, [userEmail, user.id]);
+    if (loading) {
+        return <div>Loading...</div>;
+    }
     if (userEmail === null) {
         return null;
     }
 
-    console.log("User password: ", user);
+    console.log("User firstnae: ", user.firstName);
 
     const editUser = () => {
         setIsOpen(true);
@@ -35,88 +57,21 @@ function User() {
 
     return (
         <>
-            <div flex justify-center>
-                <div className="bg-white max-w-2xl shadow overflow-hidden sm:rounded-lg">
-                    <div className="px-4 py-5 sm:px-6">
-                        <h3 className="text-lg leading-6 font-medium text-gray-900">
-                            {user.firstName + ' ' + user.lastName}
-                        </h3>
-                        <p className="mt-1 max-w-2xl text-sm text-gray-500">
-                            {user.role ? user.role.charAt(0).toUpperCase() + user.role.slice(1) : ''}
-                        </p>
-                    </div>
-                    <div className="border-t border-gray-200">
-                        <dl>
-                            <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                                <dt className="text-sm font-medium text-gray-500">
-                                    Email
-                                </dt>
-                                <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-                                    {user.email}
-                                </dd>
-                            </div>
-                            <div className="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                                <dt className="text-sm font-medium text-gray-500">
-                                    Wachtwoord
-                                </dt>
-                                <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2 flex justify-between items-center">
-                                    <span>••••••••••••</span>
-                                    <a href="#" onClick={(e) => {
-                                        e.preventDefault();
-                                    }}
-                                       className="font-medium text-[#f49700] light:text-[#f49700] hover:underline">Wachtwoord vergeten?</a>
-                                </dd>
-                            </div>
-                            <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                                <dt className="text-sm font-medium text-gray-500">
-                                    Adres
-                                </dt>
-                                <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-                                    {user.street + ' ' + user.houseNumber + ', ' + user.postalCode}
-                                </dd>
-                            </div>
-                            <div className="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                                <dt className="text-sm font-medium text-gray-500">
-                                    Stad
-                                </dt>
-                                <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-                                    {user.city}
-                                </dd>
-                            </div>
-                            <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                                <dt className="text-sm font-medium text-gray-500">
-                                    Geboortedatum
-                                </dt>
-                                <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-                                    {user.birthDate ? new Date(user.birthDate).toLocaleDateString('nl-NL') : ''}
-                                </dd>
-                            </div>
-                            <div className="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                                <dt className="text-sm font-medium text-gray-500">
-                                    btw nummer
-                                </dt>
-                                <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">                     {user.btwNumber}                 </dd>
-                            </div>
-                            <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                                <dt className="text-sm font-medium text-gray-500">
-                                    kvk nummer
-                                </dt>
-                                <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-                                    {user.kvkNumber}
-                                </dd>
-                            </div>
-                        </dl>
-                    </div>
+            <div className="grid grid-cols-2 gap-4">
+                <div>
+                    <UserProfile
+                        user={user}
+                        editUser={editUser}
+                    />
+                </div>
+                <div>
+                    <ProfileWorkshopList
+                        user={user}
+                        editUser={editUser}
+                        workshops={workshops}
+                    />
                 </div>
             </div>
-
-            {/* Bewerk button */}
-            <button
-                onClick={editUser}
-                className="bg-brand-orange hover:bg-brand-orange-hover focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-white mt-4"
-            >
-                Bewerk
-            </button>
 
             <SidePanel
                 isOpen={isOpen}
