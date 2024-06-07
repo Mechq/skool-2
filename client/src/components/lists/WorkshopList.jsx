257dd3452f6ab53934c713d71391c862c55527b4
Mathijs04 <m.vanengelen4@student.avans.nl>
2024-06-07 11:35:13 +0200

    Now the first part with data opens instead of always the first part

--- client/src/components/lists/WorkshopList.jsx
+++ client/src/components/lists/WorkshopList.jsx
@@ -17,7 +17,7 @@ export default function WorkshopList({
     const [workshopToDeleteName, setWorkshopToDeleteName] = useState(null);
     const [selectedWorkshopId, setSelectedWorkshopId] = useState(null);
     const [selectedWorkshopName, setSelectedWorkshopName] = useState(null);
-    const [isAccordionOpen, setIsAccordionOpen] = useState([true, false, false, false, false, false]);
+    const [isAccordionOpen, setIsAccordionOpen] = useState([false, false, false, false, false, false]);
 
     useEffect(() => {
         fetch('/api/workshop')
@@ -25,9 +25,19 @@ export default function WorkshopList({
             .then(data => {
                 setWorkshops(data.data);
                 console.log("Fetched workshops: ", data.data);
+                openFirstCategoryWithData(data.data);
             })
             .catch(error => console.error('Error fetching data:', error));
     }, [isOpen]);
+
+    const openFirstCategoryWithData = (workshops) => {
+        const categories = ['Beeldende kunst', 'Dans', 'Media', 'Muziek', 'Sport', 'Theater'];
+        const categorizedWorkshops = categories.map(category => workshops.filter(workshop => workshop.category === category));
+        const firstNonEmptyCategoryIndex = categorizedWorkshops.findIndex(categoryWorkshops => categoryWorkshops.length > 0);
+        if (firstNonEmptyCategoryIndex !== -1) {
+            setIsAccordionOpen(prevState => prevState.map((isOpen, i) => i === firstNonEmptyCategoryIndex));
+        }
+    };
 
     const editWorkshop = (id, e) => {
         e.preventDefault();
@@ -98,50 +108,51 @@ export default function WorkshopList({
             <div id={`accordion-collapse-body-${index}`} className={`${isAccordionOpen[index] ? '' : 'hidden'}`} aria-labelledby={`accordion-collapse-heading-${index}`}>
                 <div className="p-5 border border-b-0 border-gray-200 light:border-gray-700 light:bg-gray-900">
                     <div className="relative overflow-x-auto shadow-md sm:rounded-lg mr-6 ml-6">
-                        <table className="w-full text-sm text-left rtl:text-right text-gray-500 light:text-gray-400">
-                            <thead className="text-xs text-gray-700 uppercase bg-gray-50 light:bg-gray-700 light:text-gray-400">
-                            <tr>
-                                <th className="px-6 py-3">Workshop Naam</th>
-                                <th className="px-6 py-3">Materialen</th>
-                                <th className="px-6 py-3">Categorie</th>
-                                <th className="px-6 py-3">Bewerken</th>
-                                <th></th>
-                            </tr>
-                            </thead>
-                            <tbody>
-                            {categorizedWorkshops[category].map(workshop => (
-                                <tr key={workshop.id}
-                                    className="odd:bg-white odd:light:bg-gray-900 even:bg-gray-50 even:light:bg-gray-800 border-b light:border-gray-700 hover:cursor-pointer"
-                                    onClick={(e) => handleDetailsClick(workshop.id, workshop.name, e)}>
-                                    <td className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap light:text-white">
-                                        {"Workshop " + workshop.name}
-                                    </td>
-
-                                    <td className="px-6 py-4">{workshop.materials}</td>
-                                    <td className="px-6 py-4">{workshop.category}</td>
-
-                                    <td className="px-6 py-4">
-                                        <a href="#" onClick={(e) => {
-                                            editWorkshop(workshop.id, e);
-                                        }}
-                                           className="font-medium text-[#f49700] light:text-[#f49700] hover:underline">Bewerken</a>
-                                    </td>
-                                    <td className="px-">
-                                        <a href="#" onClick={(e) => handleDeleteClick(workshop.id, workshop.name, e)}>
-                                            <svg className="w-5 h-5 mx-2 text-danger hover:text-red-600" aria-hidden="true"
-                                                 xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none"
-                                                 viewBox="0 0 24 24">
-                                                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round"
-                                                      strokeWidth="2"
-                                                      d="M5 7h14m-9 3v8m4-8v8M10 3h4a1 1 0 0 1 1 1v3H9V4a1 1 0 0 1 1-1ZM6 7h12v13a1 1 0 0 1-1 1H7a1 1 0 0 1-1-1V7Z"/>
-                                            </svg>
-                                        </a>
-                                    </td>
+                        {categorizedWorkshops[category].length > 0 && (
+                            <table className="w-full text-sm text-left rtl:text-right text-gray-500 light:text-gray-400">
+                                <thead className="text-xs text-gray-700 uppercase bg-gray-50 light:bg-gray-700 light:text-gray-400">
+                                <tr>
+                                    <th className="px-6 py-3">Workshop Naam</th>
+                                    <th className="px-6 py-3">Materialen</th>
+                                    <th className="px-6 py-3">Categorie</th>
+                                    <th className="px-6 py-3">Bewerken</th>
+                                    <th></th>
                                 </tr>
-                            ))}
-                            </tbody>
-                        </table>
+                                </thead>
+                                <tbody>
+                                {categorizedWorkshops[category].map(workshop => (
+                                    <tr key={workshop.id}
+                                        className="odd:bg-white odd:light:bg-gray-900 even:bg-gray-50 even:light:bg-gray-800 border-b light:border-gray-700 hover:cursor-pointer"
+                                        onClick={(e) => handleDetailsClick(workshop.id, workshop.name, e)}>
+                                        <td className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap light:text-white">
+                                            {"Workshop " + workshop.name}
+                                        </td>
 
+                                        <td className="px-6 py-4">{workshop.materials}</td>
+                                        <td className="px-6 py-4">{workshop.category}</td>
+
+                                        <td className="px-6 py-4">
+                                            <a href="#" onClick={(e) => {
+                                                editWorkshop(workshop.id, e);
+                                            }}
+                                               className="font-medium text-[#f49700] light:text-[#f49700] hover:underline">Bewerken</a>
+                                        </td>
+                                        <td className="px-">
+                                            <a href="#" onClick={(e) => handleDeleteClick(workshop.id, workshop.name, e)}>
+                                                <svg className="w-5 h-5 mx-2 text-danger hover:text-red-600" aria-hidden="true"
+                                                     xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none"
+                                                     viewBox="0 0 24 24">
+                                                    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round"
+                                                          strokeWidth="2"
+                                                          d="M5 7h14m-9 3v8m4-8v8M10 3h4a1 1 0 0 1 1 1v3H9V4a1 1 0 0 1 1-1ZM6 7h12v13a1 1 0 0 1-1 1H7a1 1 0 0 1-1-1V7Z"/>
+                                                </svg>
+                                            </a>
+                                        </td>
+                                    </tr>
+                                ))}
+                                </tbody>
+                            </table>
+                        )}
                         {showModal && (
                             <div>
                                 <DeleteConfirmationModalScreen
