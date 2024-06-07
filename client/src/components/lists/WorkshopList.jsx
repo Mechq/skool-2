163ffc36baecd46dad6da0e5fb5b8893ca77106f
Mathijs04 <m.vanengelen4@student.avans.nl>
2024-06-07 11:30:09 +0200

    Workshops now get put in the right part of the accordeon

--- client/src/components/lists/WorkshopList.jsx
+++ client/src/components/lists/WorkshopList.jsx
@@ -17,7 +17,7 @@ export default function WorkshopList({
     const [workshopToDeleteName, setWorkshopToDeleteName] = useState(null);
     const [selectedWorkshopId, setSelectedWorkshopId] = useState(null);
     const [selectedWorkshopName, setSelectedWorkshopName] = useState(null);
-    const [isAccordionOpen, setIsAccordionOpen] = useState([true, false, false, false, false, false]); // State to manage accordion open/close
+    const [isAccordionOpen, setIsAccordionOpen] = useState([true, false, false, false, false, false]);
 
     useEffect(() => {
         fetch('/api/workshop')
@@ -72,11 +72,18 @@ export default function WorkshopList({
         setIsAccordionOpen(prevState => prevState.map((isOpen, i) => i === index ? !isOpen : isOpen));
     };
 
+    const categories = ['Beeldende kunst', 'Dans', 'Media', 'Muziek', 'Sport', 'Theater'];
+
+    const categorizedWorkshops = categories.reduce((acc, category) => {
+        acc[category] = workshops.filter(workshop => workshop.category === category);
+        return acc;
+    }, {});
+
     const renderAccordion = (category, index) => (
         <div key={index}>
             <h2 id={`accordion-collapse-heading-${index}`}>
                 <button type="button"
-                        className="flex items-center justify-between w-full p-5 font-medium rtl:text-right text-gray-500 border border-b-0 border-gray-200 rounded-t-xl focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-800 dark:border-gray-700 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800 gap-3"
+                        className="flex items-center justify-between w-full p-5 font-medium rtl:text-right text-gray-500 border border-b-0 border-gray-200 rounded-t-xl focus:ring-4 focus:ring-gray-200 light:focus:ring-gray-800 light:border-gray-700 light:text-gray-400 hover:bg-gray-100 light:hover:bg-gray-800 gap-3"
                         onClick={() => toggleAccordion(index)}
                         aria-expanded={isAccordionOpen[index]}
                         aria-controls={`accordion-collapse-body-${index}`}>
@@ -89,7 +96,7 @@ export default function WorkshopList({
                 </button>
             </h2>
             <div id={`accordion-collapse-body-${index}`} className={`${isAccordionOpen[index] ? '' : 'hidden'}`} aria-labelledby={`accordion-collapse-heading-${index}`}>
-                <div className="p-5 border border-b-0 border-gray-200 dark:border-gray-700 dark:bg-gray-900">
+                <div className="p-5 border border-b-0 border-gray-200 light:border-gray-700 light:bg-gray-900">
                     <div className="relative overflow-x-auto shadow-md sm:rounded-lg mr-6 ml-6">
                         <table className="w-full text-sm text-left rtl:text-right text-gray-500 light:text-gray-400">
                             <thead className="text-xs text-gray-700 uppercase bg-gray-50 light:bg-gray-700 light:text-gray-400">
@@ -102,7 +109,7 @@ export default function WorkshopList({
                             </tr>
                             </thead>
                             <tbody>
-                            {workshops.map(workshop => (
+                            {categorizedWorkshops[category].map(workshop => (
                                 <tr key={workshop.id}
                                     className="odd:bg-white odd:light:bg-gray-900 even:bg-gray-50 even:light:bg-gray-800 border-b light:border-gray-700 hover:cursor-pointer"
                                     onClick={(e) => handleDetailsClick(workshop.id, workshop.name, e)}>
@@ -163,7 +170,7 @@ export default function WorkshopList({
 
     return (
         <div id="accordion-collapse" data-accordion="collapse">
-            {['Beeldende kunst', 'Dans', 'Media', 'Muziek', 'Sport', 'Theater'].map((category, index) => renderAccordion(category, index))}
+            {categories.map((category, index) => renderAccordion(category, index))}
         </div>
     );
 }
