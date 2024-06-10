be97c88893bba36bd5a00b1801494513668e5dd0
Mech <stefrensma@gmail.com>
2024-06-10 09:47:03 +0200

    display commission date

--- client/src/components/UserWorkshopCard.jsx
+++ client/src/components/UserWorkshopCard.jsx
@@ -1,10 +1,20 @@
-export default function UserWorkshopCard({ userWorkshop }) {
+import React from "react";
+import { AiTwotoneCalendar } from "react-icons/ai";
+
+export default function UserWorkshopCard({ userWorkshop, commissionDate }) {
     return (
         <>
             <a href="#"
-               className="mb-4 block max-w-md p-10 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 light:bg-gray-800 light:border-gray-700 light:hover:bg-gray-700">
-                <h5 className="mb-4 text-3xl font-bold tracking-tight text-gray-900 light:text-white">{userWorkshop.title}</h5>
-                <p className="font-normal text-gray-700 light:text-gray-400">{userWorkshop.description}</p>
+               className="relative mb-4 block max-w-md p-10 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 light:bg-gray-800 light:border-gray-700 light:hover:bg-gray-700">
+                <h5 className="mb-4 text-3xl font-bold tracking-tight text-brand-orange light:text-white">{userWorkshop.name}</h5>
+                <p className="mb-4 font-bold text-gray-700 light:text-gray-400">{userWorkshop.category}</p>
+                <p className="mt-4 text-gray-700 light:text-gray-400">
+                    {userWorkshop.materials}
+                </p>
+                <div className="absolute bottom-4 right-4 flex items-center text-gray-700 light:text-gray-400">
+                    <AiTwotoneCalendar className="mr-2" />
+                    <span>{commissionDate}</span>
+                </div>
             </a>
         </>
     );
