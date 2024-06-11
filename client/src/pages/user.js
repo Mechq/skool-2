3585d9785f183916fff3dc44a1961d85d193c2eb
dymok <dymokilan@gmail.com>
2024-06-11 08:26:27 +0200

    userworkshops working

--- client/src/pages/user.js
+++ client/src/pages/user.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import SidePanel from "../components/SidePanel";
 import EditPanelWorkshopContent from "../components/panel-contents/EditPanelWorkshopContent";
 import UserProfile from "../components/lists/UserProfile";
@@ -6,7 +6,7 @@ import ProfileWorkshopList from "../components/lists/ProfileWorkshopList";
 import {jwtDecode} from "jwt-decode";
 
 function User() {
-    const [isOpen, setIsOpen] = React.useState(false);
+    const [isOpen, setIsOpen] = useState(false);
     const [sidePanelContent, setSidePanelContent] = useState("");
     const [user, setUser] = useState({});
     const [rotateSpan, setRotateSpan] = useState(false);
@@ -16,37 +16,42 @@ function User() {
     const [loading, setLoading] = useState(true);
 
     useEffect(() => {
+        const fetchData = async () => {
+            let decodedToken;
+            const token = localStorage.getItem('token');
+            if (token) {
+                decodedToken = jwtDecode(token);
+                setUser(decodedToken);
+            }
 
-        const token = localStorage.getItem('token');
-        if (token) {
-            const decodedToken = jwtDecode(token);
-            setUser(decodedToken);
-        }
+            try {
+                const [workshopRes, qualifiedWorkshopsRes] = await Promise.all([
+                    fetch('/api/workshop'),
+                    fetch(`/api/teacherWorkshopQualification/${decodedToken?.id}`)
+                ]);
 
-        fetch(`/api/workshop`)
-            .then(res => res.json())
-            .then(data => {
-                setWorkshops(data.data);
-                console.log("Fetched workshops: ", data.data);
-            })
-            .catch(error => console.error('Error fetching data:', error));
+                const workshopData = await workshopRes.json();
+                setWorkshops(workshopData.data);
+                // console.log("Fetched workshops: ", workshopData.data);
 
-        fetch(`/api/teacherWorkshopQualification/${user.id}`)
-            .then(res => res.json())
-            .then(data => {
-                setQualifiedWorkshops(data.data);
-                console.log("Fetched workshops: ", data.data);
-            })
-            .catch(error => console.error('Error fetching data:', error));
+                const qualifiedWorkshopsData = await qualifiedWorkshopsRes.json();
+                setQualifiedWorkshops(qualifiedWorkshopsData.data);
+                // console.log("Fetched teacherWorkshops: ", qualifiedWorkshopsData.data);
 
-        setLoading(false);
-    }, [user.id]);
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        fetchData();
+    }, []);
+
     if (loading) {
         return <div>Loading...</div>;
     }
-
-    console.log("User firstnae: ", user.firstName);
-
+    
     const editUser = () => {
         setIsOpen(true);
         setSidePanelContent("edit");
