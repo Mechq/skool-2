fd2ef82fed54f4e30e1c4495f78e96bb54a86fce
MikailSari1 <m.sari1@student.avans.nl>
2024-06-11 14:08:07 +0200

    dashboard welkom sign

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import PageSecurity from "../PageSecurity";
 import DashboardCardsCommission from '../components/DashboardCardsCommission';
 import {jwtDecode} from "jwt-decode";
 
@@ -7,6 +6,25 @@ function Home() {
     const [disableUseEffect, setDisableUseEffect] = useState(false);
     const [user, setUser] = useState({});
     const [loading, setLoading] = useState(true);
+    const [userWorkshop, setUserWorkshop] = useState({});
+    const [commissions, setCommissions] = useState([]);
+
+    const getCommissionDate = (commissionId) => {
+        const commission = commissions.find(c => c.id === commissionId);
+        console.log("commission", commission)
+        if (commission) {
+            const date = new Date(commission.date);
+            return date.toLocaleDateString('nl-NL', {
+                year: 'numeric',
+                month: 'numeric',
+                day: 'numeric',
+            });
+        }
+        return 'Unknown Date';
+    };
+
+
+
 
     useEffect(() => {
         const fetchData = async () => {
@@ -15,39 +33,37 @@ function Home() {
             if (token) {
                 decodedToken = jwtDecode(token);
                 setUser(decodedToken);
-
             }
             setLoading(false);
 
         }
         fetchData();
-        console.log("Decoded token: ", user);
     }, []);
+    
 
+    
 
 
     if (loading) {
         return <div>Loading...</div>;
     }
-else {
-        return (
-            <>
-                <div className='bg-white rounded-lg shadow-lg p-6 mb-8 text-center'>
 
-                    <h1 className='text-2xl font-bold mb-4'>
-                        Welkom <span className='text-brand-orange'>{user.name}</span>, je volgende workshop is gepland
-                        op <span className='text-brand-orange'>{user?.nextWorkshopDate}</span>
-                    </h1>
+    return (
+        <>
+        <div className='bg-white rounded-lg shadow-lg p-6 mb-8 text-center'>
+            
+        <h1 className='text-2xl font-bold mb-4'>
+    Welkom <span className='text-brand-orange'>{user.firstName}</span>, je volgende workshop is gepland op <span className='text-brand-orange'>{getCommissionDate(userWorkshop.commissionId)}</span>
+</h1>
 
-                </div>
-                <div className='block mb-2 text-sm font-medium text-gray-900 pl-6'>
-                    <h1 pd-><strong>Inschrijvingen</strong></h1>
-                    <DashboardCardsCommission user={user}/>
-                </div>
+        </div>
+        <div className='block mb-2 text-sm font-medium text-gray-900 pl-6'>
+            <h1 pd-><strong>Inschrijvingen</strong></h1>
+            <DashboardCardsCommission user={user} />
+        </div>
 
-            </>
-        );
-    }
+        </>
+    );
 }
 
 export default Home;
