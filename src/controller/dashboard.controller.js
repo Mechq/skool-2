c71b373d1522a13faba0ca5a7d8cf4d13ffdf870
MilAngEr03 <milan.bollebakker@icloud.com>
2024-06-11 11:46:54 +0200

    added backend possibility to delete enrollment

--- src/controller/dashboard.controller.js
+++ src/controller/dashboard.controller.js
@@ -24,6 +24,30 @@ let dashboardController = {
                 });
             }
         });
+    },
+
+    deleteEnrollmentById: (req, res, next) => {
+        const enrollmentId = req.params.enrollmentId;
+
+        logger.info("deleting enrollment by id", enrollmentId);
+
+        dashboardService.deleteEnrollmentById(enrollmentId, (error, success) => {
+            if (error) {
+                return next({
+                    status: error.status,
+                    message: error.message,
+                    data: {},
+                });
+            }
+
+            if (success) {
+                res.status(200).json({
+                    status: success.status,
+                    message: success.message,
+                    data: success.data,
+                });
+            }
+        });
     }
 
 };
--- src/routes/dashboard.routes.js
+++ src/routes/dashboard.routes.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const dashboardController = require("../controller/dashboard.controller");
 
 router.get('/api/dashboard/:teacherId', dashboardController.getCommissionsWorkshopsByTeacherId);
+router.delete('/api/dashboard/:enrollmentId', dashboardController.deleteEnrollmentById);
 
 module.exports = router;
