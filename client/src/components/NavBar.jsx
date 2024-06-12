274ef8fdad5c4e01afbf005fd8b9970f10703fd0
Mech <stefrensma@gmail.com>
2024-06-12 14:28:12 +0200

    display name next to icon

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -9,12 +9,13 @@ function NavBar() {
         setIsMenuOpen(!isMenuOpen);
     };
 
-    // Decode the JWT token to extract the user's role
     const token = localStorage.getItem('token');
     let userRole = null;
+    let userName = null
     if (token) {
         const decodedToken = jwtDecode(token, process.env.ACCESS_TOKEN_SECRET);
         userRole = decodedToken.role;
+        userName = decodedToken.firstName + ' ' + decodedToken.lastName;
     }
 
     return (
@@ -51,15 +52,16 @@ function NavBar() {
                         <CustomLink to="/userWorkshops" userRole={userRole}>Aanmelden</CustomLink>
                         <CustomLink to="/invites" userRole={userRole}>Uitnodigingen</CustomLink>
                         <CustomLink to="/user" userRole={userRole}>
-                            <div
-                                className="relative w-8 h-8 overflow-hidden bg-brand-orange-light rounded-full light::bg-gray-600 hover:ring-2 hover:ring-brand-orange ring-offset-2">
-                                <svg className="absolute w-10 h-10 text-brand-orange -left-1" fill="currentColor"
-                                     viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                                    <path fillRule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z"
-                                          clipRule="evenodd"></path>
-                                </svg>
+                            <div className="flex items-center">
+                                <div className="relative w-8 h-8 overflow-hidden bg-brand-orange-light rounded-full light:bg-gray-600 hover:ring-2 hover:ring-brand-orange ring-offset-2">
+                                    <svg className="absolute w-10 h-10 text-brand-orange -left-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                        <path fillRule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clipRule="evenodd"></path>
+                                    </svg>
+                                </div>
+                                <span className="ml-2">{userName}</span>
                             </div>
                         </CustomLink>
+
                     </ul>
                 </div>
             </div>
