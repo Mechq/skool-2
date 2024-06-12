65d3c796f43fe65d615084bf268dd0edc1d830ea
MikailSari1 <m.sari1@student.avans.nl>
2024-06-12 11:49:12 +0200

    stad change to woonplaats

--- client/src/components/lists/UserProfile.jsx
+++ client/src/components/lists/UserProfile.jsx
@@ -103,7 +103,7 @@ export default function UserProfile({user, editUser}) {
                             </div>
                             <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
                                 <dt className="text-sm font-medium text-gray-500">
-                                    Stad
+                                    Woonplaats
                                 </dt>
                                 <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
                                     {user.city}
