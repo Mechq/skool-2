4d21cef7a3351d043c48d966bfc997f70dc0b570
dymok <dymokilan@gmail.com>
2024-06-14 09:50:35 +0200

    profile responsiivity fixed

--- client/src/pages/profile.js
+++ client/src/pages/profile.js
@@ -56,7 +56,7 @@ export default function Profile() {
 
     return (
         <>
-            <div className="grid grid-cols-2 gap-4">
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-4 px-3 md:px-0">
                 <div>
                     <List_profile
                         user={user}
