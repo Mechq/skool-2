cebc37a22c2aaa220e7e3dbb249f694c873b8fe7
dymok <dymokilan@gmail.com>
2024-06-17 17:23:18 +0200

    minor styling

--- client/src/components/openWorkshops/ConfirmModal_openWorkshops.jsx
+++ client/src/components/openWorkshops/ConfirmModal_openWorkshops.jsx
@@ -8,7 +8,7 @@ export default function ConfirmModal_openWorkshops({ isOpen, onClose, onConfirm,
             <div className="fixed inset-0 z-10 bg-gray-900 bg-opacity-50" onClick={onClose} />
             <section className="w-full h-full flex items-center justify-center fixed top-0 left-0 z-20">
                 <div className="flex flex-col items-center justify-center w-80">
-                    <div className="w-full bg-white rounded-lg shadow dark:border dark:bg-gray-800 dark:border-gray-700">
+                    <div className="w-full bg-white rounded-lg shadow light:border light:bg-gray-800 light:border-gray-700">
                         <div className="p-6 space-y-4 sm:space-y-6">
                             <span className="close text-xl" onClick={onClose}>&times;</span>
                             <h1 className="text-xl font-medium m-0 p-0">Bevestiging</h1>
