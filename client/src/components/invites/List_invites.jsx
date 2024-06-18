e55cf04161e60fe3f8121d4db872a0213aae9fec
dymok <dymokilan@gmail.com>
2024-06-18 15:07:37 +0200

    accept / reject invite toast fixed with merge

--- client/src/components/invites/List_invites.jsx
+++ client/src/components/invites/List_invites.jsx
@@ -24,13 +24,12 @@ export default function List_invites({ invites, setInvites, user }) {
         return new Date(dateString).toLocaleDateString("nl-NL", options);
     };
 
-
     const handleConfirmAccept = () => {
         setShowAcceptModal(false);
         fetch(`/api/invite/user/${user.id}`)
             .then(res => res.json())
-            .then(data =>{
-                setInvites(data.data)
+            .then(data => {
+                setInvites(data.data);
                 setToasts(prevToasts => [...prevToasts, { id: Date.now(), type: 'success', message: 'Uitnodiging succesvol geaccepteerd' }]);
             })
             .catch(error => console.error('Error fetching data:', error));
@@ -40,7 +39,10 @@ export default function List_invites({ invites, setInvites, user }) {
         setShowRejectModal(false);
         fetch(`/api/invite/user/${user.id}`)
             .then(res => res.json())
-            .then(data => setInvites(data.data))
+            .then(data => {
+                setInvites(data.data);
+                setToasts(prevToasts => [...prevToasts, { id: Date.now(), type: 'warning', message: 'Uitnodiging geweigerd' }]);
+            })
             .catch(error => console.error('Error fetching data:', error));
     };
 
@@ -54,45 +56,44 @@ export default function List_invites({ invites, setInvites, user }) {
         setShowRejectModal(true);
     };
 
-
     return (
         <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
             <table className="w-full text-sm text-left rtl:text-right text-gray-500 light:text-gray-400">
                 <thead className="text-xs text-gray-700 uppercase bg-gray-50 light:bg-gray-700 light:text-gray-400">
-                    <tr>
-                        <th className="px-6 py-3">Workshop</th>
-                        <th className="px-6 py-3">Klant</th>
-                        <th className="px-6 py-3">Locatie</th>
-                        <th className="px-6 py-3">Datum</th>
-                        <th className="px-6 py-3">Acties</th>
-                    </tr>
+                <tr>
+                    <th className="px-6 py-3">Workshop</th>
+                    <th className="px-6 py-3">Klant</th>
+                    <th className="px-6 py-3">Locatie</th>
+                    <th className="px-6 py-3">Datum</th>
+                    <th className="px-6 py-3">Acties</th>
+                </tr>
                 </thead>
                 <tbody>
-                    {invites.map((invite, index) => (
-                        <tr
-                            key={`${invite.inviteId}-${index}`}
-                            className="odd:bg-white odd:light:bg-gray-900 even:bg-gray-50 even:light:bg-gray-800 border-b light:border-gray-700"
-                        >
-                            <td className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap light:text-white">
-                                {invite.workshopName}
-                            </td>
-                            <td className="px-6 py-4">{invite.customerName}</td>
-                            <td className="px-6 py-4">{invite.locationName}</td>
-                            <td className="px-6 py-4">{formatDate(invite.date)}</td>
-                            <td className="px-6 py-4">
-                                <button
-                                    className="bg-custom-blue hover:bg-custom-blue text-white font-bold py-2 px-4 mr-2 rounded"
-                                    onClick={() => handleAcceptClick(invite)}>
-                                    Accepteren
-                                </button>
-                                <button
-                                    className="bg-custom-red hover:bg-custom-red text-white font-bold py-2 px-4 rounded"
-                                    onClick={() => handleRejectClick(invite)}>
-                                    Weigeren
-                                </button>
-                            </td>
-                        </tr>
-                    ))}
+                {invites.map((invite, index) => (
+                    <tr
+                        key={`${invite.inviteId}-${index}`}
+                        className="odd:bg-white odd:light:bg-gray-900 even:bg-gray-50 even:light:bg-gray-800 border-b light:border-gray-700"
+                    >
+                        <td className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap light:text-white">
+                            {invite.workshopName}
+                        </td>
+                        <td className="px-6 py-4">{invite.customerName}</td>
+                        <td className="px-6 py-4">{invite.locationName}</td>
+                        <td className="px-6 py-4">{formatDate(invite.date)}</td>
+                        <td className="px-6 py-4">
+                            <button
+                                className="bg-custom-blue hover:bg-custom-blue text-white font-bold py-2 px-4 mr-2 rounded"
+                                onClick={() => handleAcceptClick(invite)}>
+                                Accepteren
+                            </button>
+                            <button
+                                className="bg-custom-red hover:bg-custom-red text-white font-bold py-2 px-4 rounded"
+                                onClick={() => handleRejectClick(invite)}>
+                                Weigeren
+                            </button>
+                        </td>
+                    </tr>
+                ))}
                 </tbody>
             </table>
 
@@ -108,7 +109,10 @@ export default function List_invites({ invites, setInvites, user }) {
             {showRejectModal && (
                 <ConfirmRejectModal_invites
                     onClose={() => setShowRejectModal(false)}
-                    onConfirm={handleConfirmReject}
+                    onConfirm={() => {
+                        handleConfirmReject();
+                        setShowRejectModal(false); // Close modal after confirming reject
+                    }}
                     invite={inviteToReject}
                     setInvites={setInvites}
                 />
