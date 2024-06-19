20f32c4abcdb418c6ce0877df8a075d03def2d41
dymok <dymokilan@gmail.com>
2024-06-19 14:11:27 +0200

    home cleanup

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -112,57 +112,56 @@ function Home() {
   const earliestDate = getEarliestDate(acceptedWorkshops);
 
   return (
-    <div className="px-3 md:px-0">
-      <div className="bg-white rounded-lg shadow-lg p-6 mb-8 text-center">
-        <h1 className="text-2xl font-bold mb-4">
-          Welkom <span className="text-brand-orange">{user.firstName}</span>,{" "}
-          {acceptedWorkshops.length > 0 ? (
-            <>
-              je volgende workshop is gepland op{" "}
-              <span className="text-brand-orange">
-                {earliestDate
-                  ? `${formatDate(earliestDate)} om ${times.startTime}`
-                  : ""}
-              </span>
-            </>
-          ) : (
-            <span> er zijn geen workshops gepland</span>
-          )}
-        </h1>
+      <div className="px-3 md:px-0">
+        <div className="bg-white rounded-lg shadow-lg p-6 mb-8 text-center">
+          <h1 className="text-2xl font-bold mb-4">
+            Welkom <span className="text-brand-orange">{user.firstName}</span>,
+            <span className="block sm:inline">{" "}</span>
+            {acceptedWorkshops.length > 0 ? (
+                <>
+                  je volgende workshop is gepland op{" "}
+                  <span className="text-brand-orange">
+          {earliestDate ? `${formatDate(earliestDate)} om ${times.startTime}` : ""}
+        </span>
+                </>
+            ) : (
+                <span> er zijn geen workshops gepland</span>
+            )}
+          </h1>
+        </div>
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
+          <div className="block mb-8 text-sm font-medium text-gray-900">
+            <h1 className="text-2xl mb-4">
+              <strong>Bevestigingen</strong>
+            </h1>
+            {acceptedWorkshops.length > 0 ? (
+                <DashboardCardsCommission userWorkshops={acceptedWorkshops}/>
+            ) : (
+                <p className="text-gray-500">Er zijn geen bevestigde workshops.</p>
+            )}
+          </div>
+          <div className="block mb-8 text-sm font-medium text-gray-900">
+            <h1 className="text-2xl mb-4">
+              <strong>Aangemelde workshops</strong>
+            </h1>
+            {signedUpWorkshops.length > 0 ? (
+                <DashboardCardsCommission userWorkshops={signedUpWorkshops}/>
+            ) : (
+                <p className="text-gray-500">Er zijn geen ingeschreven workshops.</p>
+            )}
+          </div>
+          <div className="block mb-8 text-sm font-medium text-gray-900">
+            <h1 className="text-2xl mb-4">
+              <strong>Uitgenodigde workshops</strong>
+            </h1>
+            {invitedWorkshops.length > 0 ? (
+                <DashboardCardsCommission userWorkshops={invitedWorkshops}/>
+            ) : (
+                <p className="text-gray-500">Er zijn geen uitgenodigde workshops.</p>
+            )}
+          </div>
+        </div>
       </div>
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
-        <div className="block mb-8 text-sm font-medium text-gray-900">
-          <h1 className="text-2xl mb-4">
-            <strong>Bevestigingen</strong>
-          </h1>
-          {acceptedWorkshops.length > 0 ? (
-            <DashboardCardsCommission userWorkshops={acceptedWorkshops} />
-          ) : (
-            <p>Er zijn geen bevestigde workshops.</p>
-          )}
-        </div>
-        <div className="block mb-8 text-sm font-medium text-gray-900">
-          <h1 className="text-2xl mb-4">
-            <strong>Aangemelde workshops</strong>
-          </h1>
-          {signedUpWorkshops.length > 0 ? (
-            <DashboardCardsCommission userWorkshops={signedUpWorkshops} />
-          ) : (
-            <p>Er zijn geen ingeschreven workshops.</p>
-          )}
-        </div>
-        <div className="block mb-8 text-sm font-medium text-gray-900">
-          <h1 className="text-2xl mb-4">
-            <strong>Uitgenodigde workshops</strong>
-          </h1>
-          {invitedWorkshops.length > 0 ? (
-            <DashboardCardsCommission userWorkshops={invitedWorkshops} />
-          ) : (
-            <p>Er zijn geen uitgenodigde workshops.</p>
-          )}
-        </div>
-      </div>
-    </div>
   );
 }
 
