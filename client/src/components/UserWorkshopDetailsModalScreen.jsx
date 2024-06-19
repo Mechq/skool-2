5b5c13e2db62997bf65c9fe05e6d17fe17001528
dymok <dymokilan@gmail.com>
2024-06-19 15:15:16 +0200

    modal = unfixable

--- client/src/components/UserWorkshopDetailsModalScreen.jsx
+++ client/src/components/UserWorkshopDetailsModalScreen.jsx
@@ -253,33 +253,30 @@ console.log(commission)
         <>
             <div className="fixed inset-0 z-10 bg-gray-900 bg-opacity-15" onClick={onClose}/>
 
-            <section className="w-full flex flex-col items-center justify-center fixed top-0 left-0 z-20 mt-20">
+            <section className="w-full flex flex-col items-center justify-center fixed top-0 left-0 z-20 mt-20 overflow-y-auto max-h-full">
                 <section
                     className="bg-center bg-no-repeat bg-gray-500 bg-blend-multiply w-full max-w-4xl"
                     style={{backgroundImage: `url(${workshop.picture})`}}
                 >
-                    <div className="px-4 mx-auto max-w-screen-xl text-center py-12 lg:py-24">
+                    <div className="px-4 mx-auto max-w-screen-xl text-center py-12 md:py-24">
                         <h1 className="mb-4 text-3xl font-extrabold tracking-tight leading-none text-white md:text-4xl lg:text-5xl">
                             {workshop.name}
                         </h1>
                     </div>
                 </section>
                 <div className="w-full max-w-4xl">
-                    <div
-                        className="bg-white shadow light:border light:bg-gray-800 light:border-gray-700 rounded-none">
+                    <div className="bg-white shadow light:border light:bg-gray-800 light:border-gray-700 rounded-none">
                         <div className="w-full max-w-4xl flex pb-6 pt-6">
-                            <div
-                                className={`flex-1 flex items-center justify-center border-b-2 pb-6 hover:text-gray-600  ${showWorkshopDetails ? 'border-brand-orange' : 'border-black'}`}>
+                            <div className={`flex-1 flex items-center justify-center border-b-2 pb-6 hover:text-gray-600 ${showWorkshopDetails ? 'border-brand-orange' : 'border-black'}`}>
                                 <button
-                                    onClick={() => setShowWorkshopDetails(true) && setActiveTab('workshop')}
+                                    onClick={() => { setShowWorkshopDetails(true); setActiveTab('workshop'); }}
                                 >
                                     Workshop Details
                                 </button>
                             </div>
-                            <div
-                                className={`flex-1 flex items-center justify-center border-b-2 pb-6 hover:text-gray-600 ${!showWorkshopDetails ? 'border-brand-orange' : 'border-black'}`}>
+                            <div className={`flex-1 flex items-center justify-center border-b-2 pb-6 hover:text-gray-600 ${!showWorkshopDetails ? 'border-brand-orange' : 'border-black'}`}>
                                 <button
-                                    onClick={() => setShowWorkshopDetails(false) && setActiveTab('commission')}
+                                    onClick={() => { setShowWorkshopDetails(false); setActiveTab('commission'); }}
                                 >
                                     Opdracht Details
                                 </button>
@@ -296,59 +293,42 @@ console.log(commission)
                                 <>
                                     <div className="md:w-full px-4 mb-4">
                                         <h1 className="font-bold text-4xl mb-2">{customer.name}</h1>
-                                        <div
-                                            className="flex items-center"> {/* Adding a flex container for alignment */}
+                                        <div className="flex items-center">
                                             <AiTwotoneCalendar className="mr-2"/>
                                             <p>{formatDate(commission.date)}</p>
                                         </div>
-                                        <div
-                                            className="flex items-center"> {/* Adding a flex container for alignment */}
+                                        <div className="flex items-center">
                                             <AiTwotoneClockCircle className="mr-2"/>
                                             <p>{times.startTime} - {times.endTime}</p>
                                         </div>
                                     </div>
                                     <div className="flex flex-col md:flex-row">
-                                        {/* Commissions Data */}
                                         <div className="md:w-1/2 px-4 mb-4">
                                             <h1 className="font-bold text-lg">Opdracht Informatie</h1>
                                             <p><strong>Details:</strong> {workshop.details}</p>
                                             <p><strong>Doelgroep:</strong> {workshop.targetAudience}</p>
                                             <p><strong>Leerjaar en niveau:</strong> {workshop.grade}</p>
-
                                             <p><strong>Docenten:</strong> {workshopRound.amountOfTeachers}</p>
                                             <p><strong>Leerlingen:</strong> {workshopRound.amountOfStudents}</p>
                                         </div>
-
-                                        {/* Location Data */}
                                         <div className="md:w-1/2 px-4 mb-4">
                                             <h1 className="font-bold text-lg">Locatie Informatie</h1>
                                             <p><strong>Naam:</strong> {location.name}</p>
-                                            <p>
-                                                <strong>Adres:</strong> {location.street} {location.houseNumber}, {location.city} {location.postalCode}
-                                            </p>
+                                            <p><strong>Adres:</strong> {location.street} {location.houseNumber}, {location.city} {location.postalCode}</p>
                                         </div>
                                     </div>
                                 </>
-
-
                             )}
                         </div>
-                        {!inviteState &&(
-                        <div className="flex justify-center"
-                             onClick={handleSubmit}
-                        >
-                            <button
-                                className="bg-brand-orange text-white font-bold py-4 px-8 rounded focus:outline-none hover:bg-hover-brand-orange focus:shadow-outline m-5 mt-8"
-                            >
-                                {buttonText}
-                            </button>
-                        </div>
+                        {!inviteState && (
+                            <div className="flex justify-center" onClick={handleSubmit}>
+                                <button className="bg-brand-orange text-white font-bold py-4 px-8 rounded focus:outline-none hover:bg-hover-brand-orange focus:shadow-outline m-5 mt-8">
+                                    {buttonText}
+                                </button>
+                            </div>
                         )}
                     </div>
-
                 </div>
-
-
             </section>
 
             {/* Confirmation modal for enrollment or cancellation */}
